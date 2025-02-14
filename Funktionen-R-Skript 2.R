949181e357c1e0739bc5092fecc9333ba8b612ae
PiaHerrmann <pia.herrmann@tu-dortmund.de>
2025-02-14 21:35:24 +0100

    Create Funktionen-R-Skript 2.R
    Funktionen-R-Skript 2: Allgemeine Helferfunktionen
     Dieses Skript enthält Funktionen, die in Funktionen-R-Skript 1 genutzt werden, um wiederkehrende Aufgaben wie Berechnungen, Formatierung und Visualisierung zentral zu lösen.

---
+++ Funktionen-R-Skript 2.R
@@ -0,0 +1,6 @@
+# Funktionen-R-Skript 2: Allgemeine Helferfunktionen
+# Dieses Skript enthält Funktionen, die in Funktionen-R-Skript 1 genutzt werden,
+# um wiederkehrende Aufgaben wie Berechnungen, Formatierung und Visualisierung
+# zentral zu lösen.
+# ------------------------------------------------------------------
+
